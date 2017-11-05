186c377af3eec86d6bdac81e80f206ce65ba8d90
LaAlice <larissapusch@gmx.net>
2017-11-05 22:11:19 +0100

    set bootstrap template, triggers error at wish

--- wish_todo/wish_todo/urls.py
+++ wish_todo/wish_todo/urls.py
@@ -1,6 +1,7 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = 'overview'
 urlpatterns = [
@@ -9,3 +10,5 @@ urlpatterns = [
     url(r'^todo/', include('todo.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
